9683d922baeea9c0479fedf65ea1d4bf88697fa1
Alexandre Konioukhov <alexan1@gmail.com>
2020-02-08 11:02:41 -0500

    switch

--- MenuLib/Menu.cs
+++ MenuLib/Menu.cs
@@ -8,12 +8,52 @@ namespace MenuLib
     {
         public Menu()
         {
-            ActionMenu = new List<string>
+            ActionMenu = new Dictionary<int, string>
             {
-                "Supprimer", "Quitter", "Solde", "Aide", "Facture",
+                {1, "Afficher le menu" },
+                {2, "Ajouter un produit à la commande" },
+                {3, "Supprimer un produit de la commande" },
+                {4, "Payer la facture" },
+                {5, "Afficher le solde de la facture" },
+                {6, "Afficher l'inventaire de restaurant" },
+                {7, "Aide" },
+                {8, "Quitter" },
             };
         }
         public ICollection<Product> ProductMenu { get; set; }
-        public ICollection<string> ActionMenu { get; set; }
+        public IDictionary<int, string> ActionMenu { get; set; }
+
+        public void Afficher()
+        {
+
+        }
+        public void Ajouter()
+        {
+
+        }
+        public void Payer()
+        {
+
+        }
+        public void Supprimer()
+        {
+
+        }
+        public void Quitter()
+        {
+
+        }
+        public void Solde()
+        {
+
+        }
+        public void Aide()
+        {
+
+        }
+        public void Facture()
+        {
+
+        }
     }
 }
--- Neo1/Program.cs
+++ Neo1/Program.cs
@@ -14,7 +14,46 @@ namespace Neo1
 
             foreach(var item in menu.ActionMenu)
             {
-                Console.WriteLine(item);
+                Console.Write(item.Key);
+                Console.Write(". ");
+                Console.WriteLine(item.Value);
+            }
+
+            var key = Console.ReadLine();
+
+            if (Int32.TryParse(key, out int j))
+            { 
+
+                switch (j)
+                {
+                    case 1:
+                        menu.Afficher();
+                        break;
+                    case 2:
+                        menu.Ajouter();
+                        break;
+                    case 3:
+                        menu.Supprimer();
+                        break;
+                    case 4:
+                        menu.Payer();
+                        break;
+                    case 5:
+                        menu.Solde();
+                        break;
+                    case 6:
+                        menu.Ajouter();
+                        break;
+                    case 7:
+                        menu.Aide();
+                        break;
+                    case 8:
+                        menu.Quitter();
+                        break;
+                    default:
+                        Console.WriteLine("No elements to the input");
+                        break;
+                }
             }
 
 
